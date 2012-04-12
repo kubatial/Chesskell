52c84c1057673abb0e59f2b0e4007b34e9700570
Alen Kubati <kubatial@gmail.com>
2012-04-12 03:08:18 -0400

    Print Board functionality added

--- Chesskell.hs
+++ Chesskell.hs
@@ -40,10 +40,20 @@ printRules = putStrLn (unlines ["\nWelcome to Chesskell!!!" ,
 	"-Help - Print this list of instructions again" ,
 	"Enter your choice of game or type exit: "])
 
+printHelp :: IO ()
+printHelp = putStrLn (unlines [
+	"When you enter a move you should type in the square you are" ,
+	"moving a piece from to the square you are moving to, E.g e2-e4",
+	"Finally, you have the following options: " ,
+	"-Exit - Will exit the current game" ,
+	"-Resign - you resign the current game" , 
+	"-Help - Print this list of instructions again" ,
+	"Enter your choice of game or type exit: "])
+
 parseInput :: String -> IO ()
 parseInput s
   | s == "Exit" = putStrLn "Bye!"
-  | s == "1" = humanVsHuman
+  | s == "1" = humanVsHuman initBoard
   | s == "2" = humanVsMachine1
   | s == "3" = humanVsMachine2
   | s == "4" = machineVsMachine
@@ -54,16 +64,40 @@ parseInput s
 
 ----------------------------------------------------------------------
 --Human vs Human Functionality
-humanVsHuman :: IO ()
-humanVsHuman = putStrLn "HumanvHuman"
-
---humanVsHuman = (printBoard 1 initBoard) >> (putStr "Enter Move: ")
+humanVsHuman :: Board -> IO ()
+humanVsHuman b = (printBoard 1 b) >> (putStr "Enter Move: ")
 --		>>= parseMove
 
---printBoard :: Int -> Board -> IO ()
---printBoard x b
---  | (x <= 64) && ((x `mod` 8) == 0) = putStrLn (pieceToString (b ! x))
---  | (x <= 64) = putStr (pieceToString (b ! x)) ++ 
+printBoard :: Integer -> Board -> IO ()
+printBoard x b
+  | x == 1 = putStr "8" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 9 = putStr "7" >> (putStr ("|" ++ (pieceToString (b ! x)) )) >> (printBoard (x+1) b)
+  | x == 17 = putStr "6" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 25 = putStr "5" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 33 = putStr "4" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 41 = putStr "3" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 49 = putStr "2" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | x == 57 = putStr "1" >> (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b)
+  | (x <= 64) && ((x `mod` 8) == 0) = (putStrLn ("|" ++ (pieceToString (b ! x)) ++ "|")) >> (printBoard (x+1) b)
+  | (x <= 64) = (putStr ("|" ++ (pieceToString (b ! x)))) >> (printBoard (x+1) b) 
+  | otherwise = (putStrLn "  a b c d e f g h")
+
+pieceToString :: Piece -> String
+pieceToString (Rw _) = "R"
+pieceToString (Rb _) = "r"
+pieceToString (Kw _ _) = "K"
+pieceToString (Kb _ _) = "k"
+pieceToString p
+  | p == Pw = "P"
+  | p == Pb = "p"
+  | p == Nw  = "N"
+  | p == Nb = "n"
+  | p == Bw = "B"
+  | p == Bb = "b"
+  | p == Qw = "Q"
+  | p == Qb = "q"
+  | otherwise = " "
+
 
 --parseMove :: String -> IO ()
 
