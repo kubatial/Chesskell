9432acb8c74d31a30e9aa4bbe26a8746d3d49119
astonisher <jitensuthar@gmail.com>
2012-04-12 04:04:38 -0400

    Removed ProcessAction.hs and moved function to Chesskell.hs

--- ProcessAction.hs
+++
@@ -1,5 +0,0 @@
-module ProcessAction where
-import DataStructs
-
-processAction :: Board -> Action -> IO ()
-processAction board act = putStrLn (unwords ["Call to processAction with action:", show act])
