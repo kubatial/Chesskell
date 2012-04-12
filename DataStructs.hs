b05816fddf8f25fb2286886784687f0ccbbbdea2
astonisher <jitensuthar@gmail.com>
2012-04-12 01:48:13 -0400

    Added DataStructs and initBoard in main Chesskell.hs file

---
+++ DataStructs.hs
@@ -0,0 +1,27 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module DataStructs where
+import Data.HashMap
+
+-- structure to hold piece types or 'Empty' if no piece 
+data Piece = 
+    Pw | Pb | Rw Bool | Rb Bool | Nw | Nb | Bw | Bb | Qw | Qb | Kw Bool Bool | Kb Bool Bool | Empty
+    deriving (Show)
+
+-- structure to hold various Actions types
+data  Action =
+      M Integer Integer
+    | Help
+    | Resign
+    | Exit
+    deriving (Show)
+    
+-- structure to hold game state
+data State =
+      WhiteCheck
+    | BlackCheck
+    | Nothing
+    deriving (Show)
+
+-- bord type
+type Board = Map Integer Piece
